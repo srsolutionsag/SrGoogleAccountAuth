c5732fc43da84adf86d5886c179f963c144bd892
Florian Wyss <fw@studer-raimann.ch>
2019-06-14 19:55:02 +0200

    Format login link

--- classes/class.ilSrGoogleAccountAuthUIHookGUI.php
+++ classes/class.ilSrGoogleAccountAuthUIHookGUI.php
@@ -29,10 +29,18 @@ class ilSrGoogleAccountAuthUIHookGUI extends ilUIHookPluginGUI {
 
 		if ($a_par["tpl_id"] === self::LOGIN_TEMPLATE_ID && $a_part === self::TEMPLATE_ADD) {
 
-			$html = self::output()->getHTML(self::dic()->ui()->factory()->link()->standard(self::plugin()->translate("login"), self::client()
-				->createAuthUrl()));
+			$html = $a_par["html"];
 
-			return [ "mode" => self::PREPEND, "html" => $html ];
+			$login_tpl = self::plugin()->template("login.html");
+			$login_tpl->setVariable("LINK", self::output()->getHTML(self::dic()->ui()->factory()->link()->standard(self::output()->getHTML([
+				self::dic()->ui()->factory()->icon()
+					->custom(Client::ICON_URL, self::plugin()
+						->translate("login")),
+				self::plugin()->translate("login")
+			]), self::client()->createAuthUrl())));
+			$html = str_replace('<div class="ilStartupSection">', '<div class="ilStartupSection">' . self::output()->getHTML($login_tpl), $html);
+
+			return [ "mode" => self::REPLACE, "html" => $html ];
 		}
 
 		return parent::getHTML($a_comp, $a_part, $a_par);
