7d6ef09c879e34a0516fe1066f2026f631963e40
Robin Baumgartner <robin@sr.solutions>
2022-05-06 11:12:08 +0200

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -2,8 +2,9 @@
 require_once __DIR__ . "/vendor/autoload.php";
 
 $id = "srgoogacauth";
-$version = "1.5.6";
-$ilias_min_version = "5.4.0";
-$ilias_max_version = "6.999";
-$responsible = "studer + raimann ag - Team Custom 1";
-$responsible_mail = "support-custom1@studer-raimann.ch";
+$version = "1.6.0";
+$ilias_min_version = "6.0";
+$ilias_max_version = "7.999";
+$responsible = "sr.solutions";
+$responsible_mail = "support@sr.solutions";
+$supports_cli_setup = false;
